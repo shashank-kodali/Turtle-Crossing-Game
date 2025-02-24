23dea29a9b46e78d503a47b6758ee35e79c81b0e
shashank-kodali <k.shashankvinayak@gmail.com>
2025-02-24 23:52:10 +0530

    completed the game
    created the car manger  class to create and move the cars

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ screen.tracer(0)
 screen.listen()
 
 player = Player()
-
 scoreboard = Scoreboard()
+cars = CarManager()
 
 screen.onkey(player.moveForward,"Up")
 
@@ -20,9 +20,21 @@ while game_is_on:
     time.sleep(0.1)
     screen.update()
 
+    cars.createcCar()
+    cars.moveCars()
+
     if (player.ycor() >=280):
         player.start()
         scoreboard.updateScoreBoard()
-        
+        cars.increaseSpeed()
+
+    for car in cars.all_cars:
+        if car.distance(player) < 20:
+            scoreboard.gameOver()
+            game_is_on = False
+
+
+screen.exitonclick()
+
     
 
